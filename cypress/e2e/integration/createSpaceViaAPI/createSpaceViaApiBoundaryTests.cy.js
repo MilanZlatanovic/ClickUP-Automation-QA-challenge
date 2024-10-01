05ff1d76c5432884c1fdf540ca1433961732d83b
MilanZlatanovic <milanzlatanovic92@gmail.com>
2024-10-01 19:52:00 +0200

    remove APIVerifyResponseINvalid

--- cypress/e2e/integration/createSpaceViaAPI/createSpaceViaApiBoundaryTests.cy.js
+++ cypress/e2e/integration/createSpaceViaAPI/createSpaceViaApiBoundaryTests.cy.js
@@ -1,16 +1,16 @@
 describe('Space Creation - Boundary Conditions', () => {
-   
-     //This test assumes there is validation on the backend that matches the one on the frontend
-    it('Verify response for creating space with space as name', () => {
-  
-      const spaceName = " "
-      
-      cy.APIPostSpace(spaceName).then((response) => {
-        cy.APIVerifyResponseInvalid(response)
+
+  //This test assumes there is validation on the backend that matches the one on the frontend
+  it('Verify response for creating space with space as name', () => {
+
+    const spaceName = " "
+
+    cy.APIPostSpace(spaceName).then((response) => {
+      expect(response.status).to.eq(400)
     })
-    
-      })
-  
-  
-      
-    })
+
+  })
+
+
+
+})
